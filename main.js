e03a7bb680df056576d1c3c2666ae9d8dee1912e
nemecro <nemecstudies@gmail.com>
2025-06-22 17:29:21 +0200

    create Book constructor

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 "use strict"
+
+const booksInLibrary = [];
+
+function Book(title, author, pageCount, readStatus){
+    this.title = title;
+    this.author = author;
+    this.pageCount = pageCount;
+    this.readStatus = readStatus;
+}
+
+let book1 = new Book('War and Peace', 'Leo Tolstoy', '972', true);
+
