efffe8519b24d9d03e4162daae66fd20ef9a6e71
nemecro <nemecstudies@gmail.com>
2025-06-22 17:46:31 +0200

    create renderBooks function that only appends headings and paragraphs based on the contents in booksInLibrary

--- main.js
+++ main.js
@@ -15,3 +15,22 @@ function addBookToLibrary(title = 'Untitled', author = 'Author unknown', pageCou
     booksInLibrary.push(new Book(title, author, pageCount, readStatus));
 }
 
+function renderBooks(){
+    const bookSection = document.querySelector('#bookSection');
+
+    booksInLibrary.forEach(book => {
+        const title = document.createElement('h3');
+        title.textContent = book.title;
+
+        const author = document.createElement('h4');
+        author.textContent = book.author;
+
+        const pageCount = document.createElement('p');
+        pageCount.textContent = book.pageCount;
+
+        const readStatus = document.createElement('p');
+        readStatus.textContent = book.readStatus;
+
+        bookSection.append(title, author, pageCount, readStatus);
+    })
+}
