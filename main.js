9298ed97020bcd7ec170b86327bdaf6cea3974ed
nemecro <nemecstudies@gmail.com>
2025-06-23 14:27:26 +0200

    add related code to styling of the book cards

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ function Book(title, author, pageCount, readStatus){
     this.id = crypto.randomUUID();
 }
 
-function addBookToLibrary(title = 'Untitled', author = 'Author unknown', pageCount = 'Not answered', readStatus = 'Not specified'){
+function addBookToLibrary(title = 'Untitled', author = 'Author unknown', pageCount = 'N/A', readStatus = 'N/A'){
     booksInLibrary.push(new Book(title, author, pageCount, readStatus));
 }
 
@@ -24,7 +24,11 @@ function renderBooks(){
 
 function createBookCardElements(){
     booksInLibrary.forEach(book => {
+    const bookCardContainer = document.createElement('div');
+    bookCardContainer.classList.add('bookCardContainer');
+    
     const iconContainer = document.createElement('div');
+    iconContainer.classList.add('iconContainer');
     iconContainer.innerHTML = `
       <svg width="64" height="80" viewBox="0 0 64 80" fill="none" xmlns="http://www.w3.org/2000/svg">
         <!-- Book body with rounded corners -->
@@ -36,19 +40,32 @@ function createBookCardElements(){
     </svg>
     `;
 
+    const textContainer = document.createElement('div');
+    textContainer.classList.add('textContainer');
+
     const title = document.createElement('h3');
     title.textContent = book.title;
+    title.classList.add('bookTitle');
 
     const author = document.createElement('h4');
     author.textContent = book.author;
+    author.classList.add('bookAuthor');
+
+    const minorInfoContainer = document.createElement('div');
+    minorInfoContainer.classList.add('minorInfoContainer');
 
     const pageCount = document.createElement('p');
     pageCount.textContent = book.pageCount;
+    pageCount.classList.add('bookPara');
 
     const readStatus = document.createElement('p');
     readStatus.textContent = book.readStatus;
+    readStatus.classList.add('bookPara');
 
-    bookSection.append(iconContainer, title, author, pageCount, readStatus);
+    minorInfoContainer.append(pageCount, readStatus)
+    textContainer.append(title, author, minorInfoContainer);
+    bookCardContainer.append(iconContainer, textContainer);
+    bookSection.append(bookCardContainer);
     })
 }
 
@@ -59,3 +76,8 @@ function getRandomHexColor() {
   const hexColor = '#' + randomInt.toString(16).padStart(6, '0');
   return hexColor;
 }
+
+addBookToLibrary();
+addBookToLibrary();
+addBookToLibrary();
+renderBooks();
